0d435aac34420dee78f55d6f920fb5714b44ecf6
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2020-06-19 13:34:14 +1200

    Basic control scheme implemented

--- app/src/main/java/com/example/javaassignment/MainActivity.java
+++ app/src/main/java/com/example/javaassignment/MainActivity.java
@@ -6,29 +6,33 @@ import androidx.core.view.GestureDetectorCompat;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import sokoban.Direction;
 import sokoban.Game;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView testTextView;
     private GestureDetectorCompat gestureDetectorCompat = null;
+    private Game game = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Game game = new Game();
+        game = new Game();
 
         game.addLevel("Test1", 5, 6,"######" +"#+x+.#" +"#..w.#" +"#....#" +"######");
 
-        testTextView = findViewById(R.id.testText);
+        //testTextView = findViewById(R.id.testText);
         //tv1.setText(game.toString());
 
         SwipeGestureDetector gestureListener = new SwipeGestureDetector();
@@ -37,8 +41,65 @@ public class MainActivity extends AppCompatActivity {
 
         //Adding grid layout testing
 
-        //TableLayout.LayoutParams params = new TableLayout.LayoutParams(100,100);
+        displayLevel();
+
+        Button up = findViewById(R.id.up);
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game.move(Direction.UP);
+                displayLevel();
+            }
+        });
+        Button down = findViewById(R.id.down);
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game.move(Direction.DOWN);
+                displayLevel();
+            }
+        });
+        Button left = findViewById(R.id.left);
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game.move(Direction.LEFT);
+                displayLevel();
+            }
+        });
+        Button right = findViewById(R.id.right);
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game.move(Direction.RIGHT);
+                displayLevel();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetectorCompat.onTouchEvent(event);
+        return true;
+    }
+
+
+    public void displayMessage(String message) {
+        testTextView.setText(message);
+    }
+
+    public void displayLevel() {
+
         TableLayout table = findViewById(R.id.testTable);
+
+        //Removing previous table
+        int count = table.getChildCount();
+        for (int i = 0; i < count; i++) {
+            View child = table.getChildAt(i);
+            if (child instanceof TableRow) ((ViewGroup) child).removeAllViews();
+        }
+
 
         for (int y = 0; y < game.selectedLevel.getHeight(); y++) {
 
@@ -47,7 +108,6 @@ public class MainActivity extends AppCompatActivity {
 
             for (int x = 0; x < game.selectedLevel.getWidth(); x++) {
 
-                // THIS GETS CHARACTER
                 char icon = game.selectedLevel.getXY(x,y);
 
                 ImageView cell = new ImageView(this);
@@ -77,44 +137,10 @@ public class MainActivity extends AppCompatActivity {
                         cell.setImageResource(R.drawable.crate_ontarget);
                         break;
                 }
-
-
                 row.addView(cell);
-
             }
-
             table.addView(row);
-
         }
-
-        //TableRow row = findViewById(R.id.testRow1);
-        //row.setLayoutParams(params);
-
-        //ImageView image = new ImageView(this);
-
-
-
-        //image.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-        //image.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        //image.setAdjustViewBounds(true);
-        //image.setImageResource(R.drawable.cake);
-        //image.setLayoutParams(params);
-
-        //row.addView(image);
-
-        //table.addView(row);
-
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        gestureDetectorCompat.onTouchEvent(event);
-        return true;
-    }
-
-
-    public void displayMessage(String message) {
-        testTextView.setText(message);
     }
 
 }
